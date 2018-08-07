0b2b4a9b91a7c89840bac01657ac6c5ecc3d1d06
geratsx <gerltsx@gmail.com>
2018-08-07 14:59:13 +0300

    Revert "ключить сборщик Maven[#4086]"
    This reverts commit 13fa2cd08ce7badcb9e42be60b05edd90ceb8869.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,18 +1,7 @@
 package ru.job4j.calculate;
 
-/**
-* Class Calculate для вычисления арифметических операций
-* @author MGurfinkel
-* @since 07.08.2018
-* @version 1
-*/
 public class Calculate{
 	
-	
-	/**
-	* Main
-	* @param args - args
-	*/
 	public static void main(String []args){
 		
 	System.out.println("Hello world!");
