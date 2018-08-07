1f0d510b1d340abc8915f1cd82fc56e43e08a340
geratsx <gerltsx@gmail.com>
2018-08-07 16:30:38 +0300

    1.4 JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,14 +9,23 @@ package ru.job4j.calculate;
 public class Calculate{
 	
 	
-	/**
-	* Main
-	* @param args - args
-	*/
-	public static void main(String []args){
+/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+public String echo(String name) {
+   return "Echo, echo, echo : " + name;
+}
+	
+/**
+* Main
+* @param args - args
+*/
+public static void main(String []args){
+		System.out.println("Hello world!");
 		
-	System.out.println("Hello world!");
-		
-
-	}
+}
+	
+	
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for test calculate task.
+* 
+* @author Mikhail Gurfinkel (mailto:geraltsx@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
