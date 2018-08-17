af4a9c6c47a9fb6653fae316578a8d405df70bda
geratsx <gerltsx@gmail.com>
2018-08-18 00:46:45 +0300

    убрал условие if и один return

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -14,9 +14,6 @@ public class Factorial {
      * @return Факториал.
      */
     public int calc(int n) {
-        if (n == 0) {
-            return 1;
-        }
         int result = 1;
 
         for (int i = 1; i <= n; i++) {
