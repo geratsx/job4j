f6fb4c0dedadc9c44b2330147413260b4a31900e
geratsx <gerltsx@gmail.com>
2018-08-18 14:30:28 +0300

    6.1. Классический поиск перебором. [#33489]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+/**
+ * @author Mikhail Gurfinkel (mailto:geraltsx@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class FindLoop {
+
+    public int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
