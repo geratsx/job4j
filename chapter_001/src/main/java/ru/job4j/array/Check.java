40fef974bf3f962235516be0970df5752e22b0bd
geratsx <gerltsx@gmail.com>
2018-08-26 16:04:28 +0300

    Изменена реализация согласно рекомендациям

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -14,14 +14,15 @@ public class Check {
      * @return Результат проверки. Все элементы массива равны true или все равны false - вернет true, иначе вернет false.
      */
     public boolean mono(boolean[] data) {
-        boolean result = false;
-        int counter = 0;
-        for (boolean temp : data) {
-            counter = temp ? counter + 1 : counter - 1;
+
+        boolean result = true;
+
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] != data[0]) {
+                result = false;
+                break;
+            }
         }
-       if (counter == 3 || counter == -3) {
-           result = true;
-       }
         return result;
     }
 }
