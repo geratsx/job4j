cbdeed4ea5fbec7f5da53913603d2e724c29e4c1
geratsx <gerltsx@gmail.com>
2018-09-16 20:39:55 +0300

    6.8. Удаление дубликатов в массиве. [#225] Исправлена логика работы. Добавлен второй тест.

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -15,16 +15,18 @@ public class ArrayDuplicate {
      * @param array Исходный массив.
      * @return Массив без дубликатов.
      */
-    public String[] remove(String[] array) {
-        Arrays.sort(array);
-        for (int i = 0; i < array.length - 1; i++) {
-            String tmp = array[i];
-            if (tmp.equals(array[i + 1])) {
-                array[i] = array[array.length - 1];
-                array[array.length - 1] = tmp;
-                array = Arrays.copyOf(array, array.length - 1);
+    public String[] remove(String[] array){
+        int border = array.length;
+        for (int i = 0; i < border; i++) {
+            for (int j = i + 1; j < border; j++) {
+                if (array[i].equals(array[j])) {
+                    array[j] = array[border - 1];
+                    border--;
+                    j--;
+                }
             }
         }
+        array = Arrays.copyOf(array, border);
         return array;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -9,12 +9,12 @@ package ru.job4j.array;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ArrayDuplicateTest {
     @Test
     public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
-        //напишите здесь тест, проверяющий удаление дубликатов строк из массива строк.
         ArrayDuplicate duplicate = new ArrayDuplicate();
         String[] input =  {"Привет", "Мир", "Привет", "Супер", "Мир"};
         String[] expect = {"Привет", "Мир", "Супер"};
@@ -22,5 +22,14 @@ public class ArrayDuplicateTest {
 
         assertThat(result, arrayContainingInAnyOrder(expect));
 
+    }
+
+    @Test
+    public void whenHaveFiveHiThenOneHi() {
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] input =  {"Привет", "Привет", "Привет", "Привет", "Привет"};
+        String[] expect = {"Привет"};
+        String[] result = duplicate.remove(input);
+        assertThat(result, arrayContainingInAnyOrder(expect));
     }
 }
