57c33924d7198d3b456795c5e0f9dd639da5dc1e
geratsx <gerltsx@gmail.com>
2018-11-10 16:00:50 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/tracker/StartUITest.java
+++ chapter_002/src/test/java/tracker/StartUITest.java
@@ -1,7 +1,11 @@
 package tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
 
 import static org.hamcrest.core.Is.is;
@@ -9,13 +13,114 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
 
+    /**
+     * Поле содержит дефолтный вывод в консоль.
+     */
+    private final PrintStream stdout = System.out;
+    /**
+     * Буфер для результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
+
+    /**
+     * @return Меню трекера в виде строки
+     */
+    public String getMenu() {
+        return new StringBuilder()
+        .append("Menu:")
+                .append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("0.Add new item")
+                .append(System.lineSeparator())
+                .append("1.Show all items")
+                .append(System.lineSeparator())
+                .append("2.Edit item")
+                .append(System.lineSeparator())
+                .append("3.Delete item")
+                .append(System.lineSeparator())
+                .append("4.Find item by ID")
+                .append(System.lineSeparator())
+                .append("5.Find item by name")
+                .append(System.lineSeparator())
+                .append("6.Exit")
+                .append(System.lineSeparator())
+                .append(System.lineSeparator())
+        .toString();
+    }
+
+    @Test
+    public void whenUserWontToSeeAllItemsThenPrintAllItems() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("test name1", "desc1"));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()), is(
+                new StringBuilder()
+                        .append(getMenu())
+                        .append("List of all items: ")
+                        .append(System.lineSeparator())
+                        .append(System.lineSeparator())
+                        .append("Item name='")
+                        .append(item1.getName())
+                        .append("', ")
+                        .append("desc='")
+                        .append(item1.getDesc())
+                        .append("', ")
+                        .append("id='")
+                        .append(item1.getId())
+                        .append("'")
+                        .append(System.lineSeparator())
+                        .append(System.lineSeparator())
+                        .append(getMenu())
+                        .toString()
+        ));
+    }
+
+
+    @Test
+    public void whenUserInputIdThenTrackerPrintThisItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()), is(
+                new StringBuilder()
+                        .append(getMenu())
+                        .append("-----------------Find item by Id-----------------")
+                        .append(System.lineSeparator())
+                        .append("Item name='")
+                        .append(item.getName())
+                        .append("', ")
+                        .append("desc='")
+                        .append(item.getDesc())
+                        .append("', ")
+                        .append("id='")
+                        .append(item.getId())
+                        .append("'")
+                        .append(System.lineSeparator())
+                        .append(getMenu())
+                        .toString()
+        ));
+    }
+
+
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
         Tracker tracker = new Tracker();
         Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("test name"));
-
     }
 
     @Test
