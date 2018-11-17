13343b845531fdbeb69c54b4cba9beb13993fc78
geraltsx <geraltsx@gmail.com>
2018-11-18 02:32:59 +0400

    3. if-else-throw [#49895]

--- chapter_002/src/main/java/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/tracker/ConsoleInput.java
@@ -44,10 +44,9 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of menu range. Please, select valid paragraph of menu.");
         }
+        return key;
     }
 }
