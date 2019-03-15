f736093eec110f0709675fd082ab7111e67b9627
geraltsx <geraltsx@gmail.com>
2019-03-15 22:23:17 +0400

    Отрефакторил логику очереди

--- chapter_003/src/main/java/search/PriorityQueue.java
+++ chapter_003/src/main/java/search/PriorityQueue.java
@@ -12,7 +12,6 @@ import java.util.LinkedList;
 public class PriorityQueue {
 
     private LinkedList<Task> tasks = new LinkedList<>();
-    private int elemCounter = 0;
 
     /**
      * Метод добавляет задачу в очередь в соответствии с ее приорететом {@link Task#priority}.
@@ -20,16 +19,14 @@ public class PriorityQueue {
      * @param task Новая задача, которую нужно добавить в очередь.
      */
     public void put(Task task) {
-        elemCounter++;
-        for (int i = 0; i != elemCounter; i++) {
-            if (tasks.size() == 0 || task.getPriority() < tasks.get(0).getPriority()) {
-                tasks.add(0, task);
-            } else {
-                if (task.getPriority() < tasks.get(i).getPriority()) {
-                    tasks.add(i - 1, task);
-                }
+        int count = tasks.size();
+        for (int i = 0; i < tasks.size(); i++) {
+            if (task.getPriority() < tasks.get(i).getPriority()) {
+                count = i;
+                break;
             }
         }
+        tasks.add(count, task);
     }
 
     /**
